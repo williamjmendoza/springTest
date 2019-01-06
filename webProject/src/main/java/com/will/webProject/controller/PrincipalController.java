f855d736526c3f6ef67348d7f8d1778a9c1a9e30
William Mendoza <williamjmendoza@gmail.com>
2019-01-05 23:39:15 -0300

    Se agrega project base y settings

--- webProject/src/main/java/com/will/webProject/controller/PrincipalController.java
+++ webProject/src/main/java/com/will/webProject/controller/PrincipalController.java
@@ -1,15 +1,22 @@
 package com.will.webProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrincipalController
 {
+	@Autowired
+	@Value(value = "${specific.variable.test}")
+	String specificVariableTest;
+	
 	@RequestMapping(name="/test")
 	public String test()
 	{
 		System.out.printf("Hola desde Spring%n");
+		System.out.printf("specificVariableTest: %s%n", specificVariableTest);
 		
 		return "Hola";
 	}
